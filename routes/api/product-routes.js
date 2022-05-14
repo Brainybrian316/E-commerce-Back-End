1e8147fe38d5b6d1e75406c0cbec7b0c7fc525a9
Brainybrian316 <brainybrian316@gmail.com>
2022-05-14 14:15:27 -0400

    router post product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -70,10 +70,9 @@ router.post('/', (req, res) => {
     }
   */
   Product.create({
-    product_name: req.body.product_name,
+    name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    category_id: req.body.category_id,
     tagIds: req.body.tagIds
   })
     .then((product) => {
