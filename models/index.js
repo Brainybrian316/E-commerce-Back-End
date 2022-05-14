55537eb15d2897787d0cb5d0a90bba56fd37dd95
Brainybrian316 <brainybrian316@gmail.com>
2022-05-14 13:01:20 -0400

    edited index.js

--- models/index.js
+++ models/index.js
@@ -11,15 +11,15 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id'
-})
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag
-})
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag
-})
+});
 
 module.exports = {
   Product,
